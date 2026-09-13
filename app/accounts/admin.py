9b115e83f5c84555115e43ab2ff642dd52335725
Oyips <107349797+Oyips@users.noreply.github.com>
2026-09-13 20:15:56 +0100

    update app/accounts/admin.py

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
-admin.site.register(User, UserAdmin)
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role & School', {'fields': ('role', 'school')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Role & School', {'fields': ('role', 'school')}),
+    )
+    list_display = ('username', 'email', 'role', 'school', 'is_staff')
+
+
+admin.site.register(User, CustomUserAdmin)
