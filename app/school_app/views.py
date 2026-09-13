16e2c145438d3af814c915dbd12984aa93cc78fc
Oyips <107349797+Oyips@users.noreply.github.com>
2026-09-13 20:18:45 +0100

    update app/school_app/views.py

--- app/school_app/views.py
+++ app/school_app/views.py
@@ -1,7 +1,7 @@
-# school_app/views.py
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import School, Class, Student
+
 
 @login_required
 def dashboard(request):
