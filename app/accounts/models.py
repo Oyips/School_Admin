c39c1d4492a373c66eec2b163e2e74826e8ebb12
Oyips <107349797+Oyips@users.noreply.github.com>
2026-09-13 16:43:13 +0100

    update app/accounts/models.py

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Create your models here.# accounts/models.py
+
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('platform_admin', 'Platform Admin'),
+        ('school_owner', 'School Owner'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    school = models.ForeignKey('schools.School', null=True, blank=True,
+                                on_delete=models.CASCADE, related_name='members')
