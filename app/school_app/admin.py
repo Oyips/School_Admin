e513405bc75c34bbd83ed6303e67d0a0ad67e8e6
Oyips <107349797+Oyips@users.noreply.github.com>
2026-09-13 20:17:52 +0100

    update app/school_app/admin.py

--- app/school_app/admin.py
+++ app/school_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import School, Class, Student, Attendance, Grade
+
 admin.site.register(School)
 admin.site.register(Class)
 admin.site.register(Student)
