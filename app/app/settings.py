763b42bc0f819c9407157105b88cbe2b450399a6
Oyips <107349797+Oyips@users.noreply.github.com>
2026-09-13 21:28:25 +0100

    update app/app/settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 
 
 LOGIN_REDIRECT_URL = '/dashboard/'
+LOGIN_URL = '/login/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
