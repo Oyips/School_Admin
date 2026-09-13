36319920881322fa3a9e7cac9151f2980af56507
Oyips <107349797+Oyips@users.noreply.github.com>
2026-09-13 16:49:14 +0100

    update app/app/settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'school_app',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+AUTH_USER_MODEL = 'accounts.User'   # critical — must be set BEFORE first migrate
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
